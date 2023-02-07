4afea5eb7eeda54b65d961acee6a578f38273999
jiemin-rgb <80969356+jiemin-rgb@users.noreply.github.com>
2023-02-07 21:25:49 +0800

    Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema({
 });
 
 // Encryption
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] }); //Pluggin: give extra features. Giving encryption power. 
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +38,7 @@ app.get("/login", function(req, res) {
 app.post("/login", function(req, res) {
     //Check if credentials are in database
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password); //hash password
 
     User.findOne({ email: username }, function(err, foundUser) {
         if (err) {
@@ -64,7 +63,7 @@ app.post("/register", function(req, res) {
     // save new user details
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password) //hash password
     });
 
     newUser.save(function(err) {
